5bcca3022d1aecd34888e4aa32f814ac28432c0b
Jimmy Lee <rainbean@users.noreply.github.com>
2021-02-08 15:06:29 +0800

    Create build.ps1

---
+++ scripts/build.ps1
@@ -0,0 +1,9 @@
+$env:Path += ";C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64;C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0/x64"
+
+7z -h
+
+signtool -h
+
+makeappx -h
+
+echo "done
